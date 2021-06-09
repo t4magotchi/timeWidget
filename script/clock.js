736349c0bb86dfc929fd53b109c8aa002b48daae
Sergey <dzzisperfect@gmail.com>
2021-06-09 20:26:33 +0300

    Operator full

--- script/clock.js
+++ script/clock.js
@@ -6,7 +6,7 @@ function getTime(){
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}`: minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
 }
 function init(){
     getTime();
